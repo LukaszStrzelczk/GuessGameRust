0edac992edd1c436b183d6f451a2eac3e7254449
Łukasz Strzelczyk <lstrzelczyk.business@gmail.com>
2024-03-07 16:01:38 +0100

    rand added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-<<<<<<< HEAD
-
-
-fn main() {
-    println!("Hello, world!");
-    
-=======
+//to input from console
 use std::io;
+//for randomization
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
+    
+    //this is secret number you are supposed to guess it ranges form 1 to 100 inclusively
+    let secret_guess=rand::thread_rng().gen_range(1..=100);
+    //displaying secret number
+    println!("{secret_guess}");
+    println!("please enter your guuess");
+    //empty string for user guess
     let mut guess = String::new();
+
+    //function that takes input
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line");
     println!("{guess}");
->>>>>>> 0cf40bbc27f41d1e5bec12cf9879c6e588e2f219
 }
